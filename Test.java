4ed09e5fc4bd0d3ca44637815c030c1fa845ee1b
Jacob <chenshyfool@qq.com>
2014-11-16 10:41:54 -0600

    revise to fullfil the instruction

--- Test.java
+++ Test.java
@@ -1,52 +1,76 @@
 public class Test {
 
 	public static void main(String[] args) {
-		MapDSP dsp;
 		EdgeWeightedDigraph g;
-		int V;
-		int trials;
+		int V = 0;
+		int E = 0;
+		int count = 0;
 
-		In in = new In(args[0]);
-		trials = Integer.parseInt(args[1]);
-
-		V = Integer.parseInt(in.readLine().trim().split("\\s+")[0]);
+		Readfile in = new Readfile("input.txt");
+		
+        String[] next1 = in.readLine().trim().split("\\s+");
+		V = Integer.parseInt(next1[0]);
+		E = Integer.parseInt(next1[1]);
+		
 		Point2D[] places = new Point2D[V];
 		g = new EdgeWeightedDigraph(V);
 
 		int p1;
 		int p2;
+		int s;
+		int d;
 		double dist;
-		while (!in.isEmpty()) {
-			String[] next = in.readLine().trim().split("\\s+");
-			// split with white space as delimiter
-
-			if (next.length == 3) {
-				places[Integer.parseInt(next[0])] = new Point2D(
-						Double.parseDouble(next[1]),
-						Double.parseDouble(next[2]));
+		
+		while (!in.isEmpty()) {	
+			String[] next2 = in.readLine().trim().split("\\s+");
+			if (next2.length == 3) {
+				places[Integer.parseInt(next2[0])] = new Point2D(
+						Double.parseDouble(next2[1]),
+						Double.parseDouble(next2[2]));
 			}
 
-			if (next.length == 2) {
-				p1 = Integer.parseInt(next[0]);
-				p2 = Integer.parseInt(next[1]);
+			if (next2.length == 2 && count < E) {
+				count++;
+				p1 = Integer.parseInt(next2[0]);
+				p2 = Integer.parseInt(next2[1]);
 				dist = places[p1].distanceTo(places[p2]);
 				g.addEdge(new DirectedEdge(p1, p2, dist));
 				g.addEdge(new DirectedEdge(p2, p1, dist));
 			}
-		}
-
-		dsp = new MapDSP(g, places, -1);
-		for (int i = 0; i < trials; i++) {
-			int s = (int) (Math.random() * V);
-			int d = (int) (Math.random() * V);
-			StdOut.println("from " + s + " to " + d + " : "
-					+ dsp.shortestPath(s, d));
-
-			if (dsp.hasPathTo(d)) {
-				for (DirectedEdge e : dsp.pathTo(d))
-					StdOut.print(e.from() + " --> ");
-				StdOut.println(d);
+			
+			if (next2.length == 2 && count >= E){
+				Dijkstra dsp = new Dijkstra(g, places, -1);
+				s = Integer.parseInt(next2[0]);
+				d = Integer.parseInt(next2[1]);
+				System.out.println(dsp.shortestPath(s, d));
+				if (dsp.hasPathTo(d)) {
+					System.out.print("[");
+					for (DirectedEdge e : dsp.pathTo(d))
+						System.out.print(e.from() + ",");
+					System.out.println(d+ "]");
+				}
+				count++;
 			}
 		}
+
+		/*dsp = new Dijkstra(g, places, -1);
+		int s1 = 0;
+		int d1 = 5;
+		int s2 = 2;
+		int d2 = 3;
+		System.out.println(dsp.shortestPath(s1, d1));
+		if (dsp.hasPathTo(d1)) {
+			System.out.print("[");
+			for (DirectedEdge e : dsp.pathTo(d1))
+				System.out.print(e.from() + ",");
+			System.out.println(d1+ "]");
+		}
+		System.out.println(dsp.shortestPath(s2, d2));
+		if (dsp.hasPathTo(d2)) {
+			System.out.print("[");
+			for (DirectedEdge e : dsp.pathTo(d2))
+				System.out.print(e.from() + ",");
+			System.out.println(d2+ "]");
+		}*/
 	}
 }
